3770934d09def4869ce38455d6eaf219b5386253
Sebastián Solorzano <ssolorzano10@gmail.com>
2023-05-03 03:13:37 -0600

    Header terminado

--- proyecto1/src/components/Header.js
+++ proyecto1/src/components/Header.js
@@ -6,6 +6,12 @@ import watchlist from '../images/watchlist.svg'
 import original from '../images/original.svg'
 import movies from '../images/movie.svg'
 import series from '../images/series.svg'
+import spiderman from '../images/spiderman.png'
+import vision from '../images/vision.png'
+import wanda from '../images/wanda.png'
+import grogu from '../images/grogu.png'
+import loki from '../images/loki.png'
+import chi from '../images/Chi.png'
 import './Styles/Header.css'
 
 export default class Header extends Component{
@@ -45,6 +51,49 @@ export default class Header extends Component{
                             <span>SERIES</span>
                         </a>
                     </nav>
+
+                    <div class="profile">
+                        <div class="profile-selected">
+                            <p id="name-user">Sebas</p>
+                            <img class="avatar img-user" src={spiderman} alt="spiderman"/>
+                        </div>
+                        
+                        <nav class="profile-nav">
+                            <ul class="profile-nav-profiles">
+                                <li>
+                                    <img class="avatar" src={vision} alt="vision"/>
+                                    <p>Master</p>
+                                </li>
+                                <li>
+                                    <img class="avatar" src={wanda} alt="wanda"/>
+                                    <p>Tiviet</p>
+                                </li>
+                                <li>
+                                    <img class="avatar" src={grogu} alt="grogu"/>
+                                    <p>Valdez</p>
+                                </li>
+                                <li>
+                                    <img class="avatar" src={loki} alt="Loki"/>
+                                    <p>Manuel</p>
+                                </li>
+                                <li>
+                                    <img class="avatar" src={chi} alt="chi"/>
+                                    <p>Ludwing</p>
+                                </li>
+                                <div class="opciones">
+                                    <li><a href='/editar perfiles'><span>Editar perfiles</span></a></li>
+                                    <li><a href='/ajustes'><span>Ajustes de aplicación</span></a></li>
+                                    <li><a href='/cuenta'><span>Cuenta</span></a></li>
+                                    <li><a href='/ayuda'><span>Ayuda</span></a></li>
+                                    <li><a href='/cerrar sesion'><span>Cerrar sesión</span></a></li>
+                                </div>
+                                
+                            </ul>
+                            <ul class="profile-nav-options">
+                                
+                            </ul>
+                        </nav>
+                    </div>
                 </nav>
             </header>
         );
