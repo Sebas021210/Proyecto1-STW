17bdd7f4bb408f766ad121ee6822fd58626910d3
Sebastián Solorzano <ssolorzano10@gmail.com>
2023-05-02 19:32:38 -0600

    Cambios Index&App

--- proyecto1/src/App.js
+++ proyecto1/src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component{
+    render(){
+        return(
+            <div id="App">
+            </div>
+        );
+    }
 }
 
 export default App;
