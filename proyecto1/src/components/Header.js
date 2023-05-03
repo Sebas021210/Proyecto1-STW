7c162fdd011f242ddc7bbdcf27ff809f613ecf87
Sebastián Solorzano <ssolorzano10@gmail.com>
2023-05-02 23:13:59 -0600

    Header

--- proyecto1/src/components/Header.js
+++ proyecto1/src/components/Header.js
@@ -12,39 +12,39 @@ export default class Header extends Component{
     render(){
         return(
             <header>
-                <logo>
-                    <img src={logo} alt="Logo Disney+" />
-                </logo>
                 <nav>
-                    <a href='/inicio'>
-                        <img src={home} alt='INICIO'/>
-                        <span>INICIO</span>
-                    </a>
+                    <img src={logo} alt="Logo Disney+" />
+                    <nav id="Menu">
+                        <a href='/inicio'>
+                            <img src={home} alt='INICIO'/>
+                            <span>INICIO</span>
+                        </a>
 
-                    <a href='/busqueda'>
-                        <img src={search} alt='BÚSQUEDA'/>
-                        <span>BÚSQUEDA</span>
-                    </a>
-                    
-                    <a href='/lista'>
-                        <img src={watchlist} alt='MI LISTA'/>
-                        <span>MI LISTA</span>
-                    </a>
-                    
-                    <a href='/originales'>
-                        <img src={original} alt='ORIGINALES'/>
-                        <span>ORIGINALES</span>
-                    </a>
+                        <a href='/busqueda'>
+                            <img src={search} alt='BÚSQUEDA'/>
+                            <span>BÚSQUEDA</span>
+                        </a>
+                        
+                        <a href='/lista'>
+                            <img src={watchlist} alt='MI LISTA'/>
+                            <span>MI LISTA</span>
+                        </a>
+                        
+                        <a href='/originales'>
+                            <img src={original} alt='ORIGINALES'/>
+                            <span>ORIGINALES</span>
+                        </a>
 
-                    <a href='/peliculas'>
-                        <img src={movies} alt='PELÍCULAS'/>
-                        <span>PELÍCULAS</span>
-                    </a>
-                    
-                    <a href='/series'>
-                        <img src={series} alt='SERIES'/>
-                        <span>SERIES</span>
-                    </a>
+                        <a href='/peliculas'>
+                            <img src={movies} alt='PELÍCULAS'/>
+                            <span>PELÍCULAS</span>
+                        </a>
+                        
+                        <a href='/series'>
+                            <img src={series} alt='SERIES'/>
+                            <span>SERIES</span>
+                        </a>
+                    </nav>
                 </nav>
             </header>
         );
