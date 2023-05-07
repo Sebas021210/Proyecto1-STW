28091832ef226e782c2bf10869a51f1ce67d41fb
Sebastián Solorzano <ssolorzano10@gmail.com>
2023-05-07 01:38:39 -0600

    :(

--- proyecto1/src/components/componentsBody/Movies.js
+++ proyecto1/src/components/componentsBody/Movies.js
@@ -4,6 +4,11 @@ import img2SW from '../../images/starwars/img2.jpg'
 import img3SW from '../../images/starwars/img3.jpg'
 import img4SW from '../../images/starwars/img4.jpg'
 import img5SW from '../../images/starwars/img5.jpg'
+import img6SW from '../../images/starwars/img6.jpg'
+import img7SW from '../../images/starwars/img7.jpg'
+import img8SW from '../../images/starwars/img8.jpg'
+import img9SW from '../../images/starwars/img9.jpg'
+import img10SW from '../../images/starwars/img10.jpg'
 
 import img1N from '../../images/novedades/img1.jpg'
 import img2N from '../../images/novedades/img2.jpg'
@@ -16,6 +21,11 @@ import img2IC from '../../images/iconos/img2.jpg'
 import img3IC from '../../images/iconos/img3.jpg'
 import img4IC from '../../images/iconos/img4.jpg'
 import img5IC from '../../images/iconos/img5.jpg'
+import img6IC from '../../images/iconos/img6.jpg'
+import img7IC from '../../images/iconos/img7.jpg'
+import img8IC from '../../images/iconos/img8.jpg'
+import img9IC from '../../images/iconos/img9.jpg'
+import img10IC from '../../images/iconos/img10.jpg'
 
 import img1R from '../../images/recomendaciones/img1.jpg'
 import img2R from '../../images/recomendaciones/img2.jpg'
@@ -25,58 +35,50 @@ import img5R from '../../images/recomendaciones/img5.jpg'
 
 import prev from '../../images/prev.png'
 import next from '../../images/next.png'
-
 import '../Styles/Movies.css'
 
 export default class Movies extends Component {
-    /* 
-    componentDidMount() {
-        let cardContainers = [...document.querySelectorAll('.card-container-grande')];
-      
-        cardContainers.forEach((container) => {
-            let preButton = container.querySelector('.prevBtn');
-            let nextButton = container.querySelector('.nextBtn');
-      
-            if (preButton && nextButton) {
-                let containerDimensions = container.getBoundingClientRect();
-                let containerWidth = containerDimensions.width;
-      
-                nextButton.addEventListener('click', () => {
-                    container.scrollLeft += containerWidth - 200;
-                });
-      
-                preButton.addEventListener('click', () => {
-                    container.scrollLeft -= containerWidth + 200;
-                });
-            }
-        });
-    }      
-    */
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: 0,
+        }
+    }
+
+    handlePrevClick = () => {
+        const { currentPage } = this.state;
+        if (currentPage > 0) {
+            this.setState({ currentPage: currentPage - 5 });
+        }
+    }
+
+    handleNextClick = () => {
+        const { currentPage } = this.state;
+        if (currentPage < 5) {
+            this.setState({ currentPage: currentPage + 5 });
+        }
+    }
 
     render(){
+        const { currentPage } = this.state;
+        const visibleMoviesSW = [img1SW, img2SW, img3SW, img4SW, img5SW, img6SW, img7SW, img8SW, img9SW, img10SW].slice(currentPage, currentPage + 5);
+        const visibleMoviesN = [img1N, img2N, img3N, img4N, img5N, img1R, img2R, img3R, img4R, img5R].slice(currentPage, currentPage + 5);
+        const visibleMoviesIC = [img1IC, img2IC, img3IC, img4IC, img5IC, img6IC, img7IC, img8IC, img9IC, img10IC].slice(currentPage, currentPage + 5);
+        const visibleMoviesR = [img1R, img2R, img3R, img4R, img5R, img1N, img2N, img3N, img4N, img5N].slice(currentPage, currentPage + 5);
+
         return(
             <div className="container">
                 <div className="movies">
                     <h1 className="title">Star Wars: Películas y series</h1>
                     <div className="movies-list-grande">
-                        <button className="prevBtn"><img src={prev} alt="" /></button>
-                        <button className="nextBtn"><img src={next} alt="" /></button>
+                        <button className="prevBtn" onClick={this.handlePrevClick}><img src={prev} alt="" /></button>
+                        <button className="nextBtn" onClick={this.handleNextClick}><img src={next} alt="" /></button>
                         <div className="card-container-grande">
-                            <div className="card-grande">
-                                <img src={img1SW} className="card-img-grande" alt="" />
-                            </div>
-                            <div className="card-grande">
-                                <img src={img2SW} className="card-img-grande" alt="" />
-                            </div>
-                            <div className="card-grande">
-                                <img src={img3SW} className="card-img-grande" alt="" />
-                            </div>
-                            <div className="card-grande">
-                                <img src={img4SW} className="card-img-grande" alt="" />
-                            </div>
-                            <div className="card-grande">
-                                <img src={img5SW} className="card-img-grande" alt="" />
-                            </div>
+                            {visibleMoviesSW.map(movie => (
+                                <div className="card-grande">
+                                    <img src={movie} className="card-img-grande" alt="" />
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
@@ -84,24 +86,14 @@ export default class Movies extends Component {
                 <div className="movies">
                     <h1 className="title">Novedades en Disney+</h1>
                     <div className="movies-list-peque">
-                        <button className="prevBtn"><img src={prev} alt="" /></button>
-                        <button className="nextBtn"><img src={next} alt="" /></button>
+                        <button className="prevBtn" onClick={this.handlePrevClick}><img src={prev} alt="" /></button>
+                        <button className="nextBtn" onClick={this.handleNextClick}><img src={next} alt="" /></button>
                         <div className="card-container-peque">
-                            <div className="card-peque">
-                                <img src={img1N} className="card-img-peque" alt="" />
-                            </div>
-                            <div className="card-peque">
-                                <img src={img2N} className="card-img-peque" alt="" />
-                            </div>
-                            <div className="card-peque">
-                                <img src={img3N} className="card-img-peque" alt="" />
-                            </div>
-                            <div className="card-peque">
-                                <img src={img4N} className="card-img-peque" alt="" />
-                            </div>
-                            <div className="card-peque">
-                                <img src={img5N} className="card-img-peque" alt="" />
-                            </div>
+                            {visibleMoviesN.map(movie => (
+                                <div className="card-peque">
+                                    <img src={movie} className="card-img-peque" alt="" />
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
@@ -109,24 +101,14 @@ export default class Movies extends Component {
                 <div className="movies">
                     <h1 className="title">Íconos del pop</h1>
                     <div className="movies-list-grande">
-                    <button className="prevBtn"><img src={prev} alt="" /></button>
-                    <button className="nextBtn"><img src={next} alt="" /></button>
+                        <button className="prevBtn" onClick={this.handlePrevClick}><img src={prev} alt="" /></button>
+                        <button className="nextBtn" onClick={this.handleNextClick}><img src={next} alt="" /></button>
                         <div className="card-container-grande">
-                            <div className="card-grande">
-                                <img src={img1IC} className="card-img-grande" alt="" />
-                            </div>
-                            <div className="card-grande">
-                                <img src={img2IC} className="card-img-grande" alt="" />
-                            </div>
-                            <div className="card-grande">
-                                <img src={img3IC} className="card-img-grande" alt="" />
-                            </div>
-                            <div className="card-grande">
-                                <img src={img4IC} className="card-img-grande" alt="" />
-                            </div>
-                            <div className="card-grande">
-                                <img src={img5IC} className="card-img-grande" alt="" />
-                            </div>
+                            {visibleMoviesIC.map(movie => (
+                                <div className="card-grande">
+                                    <img src={movie} className="card-img-grande" alt="" />
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
@@ -134,24 +116,14 @@ export default class Movies extends Component {
                 <div className="movies">
                     <h1 className="title">Recomendaciones para ti</h1>
                     <div className="movies-list-peque">
-                        <button className="prevBtn"><img src={prev} alt="" /></button>
-                        <button className="nextBtn"><img src={next} alt="" /></button>
+                        <button className="prevBtn" onClick={this.handlePrevClick}><img src={prev} alt="" /></button>
+                        <button className="nextBtn" onClick={this.handleNextClick}><img src={next} alt="" /></button>
                         <div className="card-container-peque">
-                            <div className="card-peque">
-                                <img src={img1R} className="card-img-peque" alt="" />
-                            </div>
-                            <div className="card-peque">
-                                <img src={img2R} className="card-img-peque" alt="" />
-                            </div>
-                            <div className="card-peque">
-                                <img src={img3R} className="card-img-peque" alt="" />
-                            </div>
-                            <div className="card-peque">
-                                <img src={img4R} className="card-img-peque" alt="" />
-                            </div>
-                            <div className="card-peque">
-                                <img src={img5R} className="card-img-peque" alt="" />
-                            </div>
+                            {visibleMoviesR.map(movie => (
+                                <div className="card-peque">
+                                    <img src={movie} className="card-img-peque" alt="" />
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
--- proyecto1/src/components/componentsBody/Slide.js
+++ proyecto1/src/components/componentsBody/Slide.js
@@ -15,47 +15,38 @@ import '../Styles/Slide.css'
 const movies = [
   {
     name: 'Movie 1',
-    des: 'This is the first movie',
     image: img1
   },
   {
     name: 'Movie 2',
-    des: 'This is the second movie',
     image: img2
   },
   {
     name: 'Movie 3',
-    des: 'This is the third movie',
     image: img3
   },
   {
     name: 'Movie 4',
-    des: 'This is the fourth movie',
     image: img4
   },
   {
     name: 'Movie 5',
-    des: 'This is the fifth movie',
     image: img5
   },
   {
     name: 'Movie 6',
-    des: 'This is the sixth movie',
     image: img6
   },
   {
     name: 'Movie 7',
-    des: 'This is the seventh movie',
     image: img7
   },
   {
     name: 'Movie 8',
-    des: 'This is the eighth movie',
     image: img8
   },
   {
     name: 'Movie 9',
-    des: 'This is the nine movie',
     image: img9
   },
 ];
@@ -64,81 +55,77 @@ export default class Slide extends Component {
   componentDidMount() {
     const carousel = document.querySelector('.carousel');
     //const prevButton = document.querySelector('.prev');
-    //const nextButton = document.querySelector('.next');
+    const nextButton = document.querySelector('.next');
     let sliders = [];
     let slideIndex = 0;
-
+    let intervalId = null;
+  
     const createSlide = () => {
       if (slideIndex >= movies.length) {
         slideIndex = 0;
       }
-
+  
       let slide = document.createElement('div');
       let imgElement = document.createElement('img');
       let content = document.createElement('div');
       let h1 = document.createElement('h1');
-      let p = document.createElement('p');
-
+  
       imgElement.appendChild(document.createTextNode(''));
       h1.appendChild(document.createTextNode(movies[slideIndex].name));
-      p.appendChild(document.createTextNode(movies[slideIndex].des));
       content.appendChild(h1);
-      content.appendChild(p);
       slide.appendChild(imgElement);
       slide.appendChild(content);
       carousel.appendChild(slide);
-
+  
       imgElement.src = movies[slideIndex].image;
       slideIndex++;
-
+  
       slide.className = 'slider';
       content.className = 'slide-content';
       h1.className = 'title';
-      p.className = 'movie';
-
+  
       sliders.push(slide);
-
+  
       if (sliders.length) {
         sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${30 * (sliders.length - 2)}px)`;
       }
     }
-
-    /* 
-    const showSlides = () => {
-      if (slideIndex >= sliders.length) {
-        slideIndex = 0;
-      } else if (slideIndex < 0) {
-        slideIndex = sliders.length - 1;
-      }
   
-      for (let i = 0; i < sliders.length; i++) {
-        sliders[i].style.display = "none";
-      }
-  
-      sliders[slideIndex].style.display = "block";
-    };
-
-    showSlides();
-  
-    nextButton.addEventListener("click", () => {
-      slideIndex++;
-      showSlides();
-    });
-  
-    prevButton.addEventListener("click", () => {
-      slideIndex--;
-      showSlides();
-    });
-    */
-
     for (let i = 0; i < 3; i++) {
       createSlide();
     }
-
-    setInterval(() => {
+  
+    nextButton.addEventListener("click", () => {
+      slideIndex++;
+      createSlide();
+      clearInterval(intervalId);
+      intervalId = setInterval(() => {
+        createSlide();
+      }, 5000);
+    });
+  
+    /* 
+    prevButton.addEventListener("click", () => {
+      slideIndex--;
+      if (slideIndex < 0) {
+        slideIndex = movies.length - 1;
+      }
+      sliders = [];
+      for (let i = 0; i < 3; i++) {
+        createSlide();
+      }
+      clearInterval(intervalId);
+      intervalId = setInterval(() => {
+        createSlide();
+      }, 5000);
+    });
+    */
+  
+    intervalId = setInterval(() => {
       createSlide();
     }, 5000);
   }
+  
 
   render() {
     return (
