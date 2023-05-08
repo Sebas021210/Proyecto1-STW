b3a9daa0875207cba79317ea2f82652447f577a9
Sebastián Solorzano <ssolorzano10@gmail.com>
2023-05-08 08:50:48 -0600

    Webpack&Babel

---
+++ proyecto1/webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist_webpack_babel'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+      rules: [
+        {
+            test: /\.css$/,
+            use: [
+              MiniCssExtractPlugin.loader,
+              'css-loader'
+            ]
+        },
+        {
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader'
+            ]
+        },
+        {
+            test: /\.(png|jpe?g|gif|svg|webp)$/i,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'img/'
+                    }
+                }
+            ]
+        },
+        {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        },
+      ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            filename: 'index.html',
+            template: './public/index.html',
+            chunks: ['index']
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new CssMinimizerPlugin()
+    ],
+    optimization: {
+        minimizer: [new CssMinimizerPlugin(),],
+    },
+};
